0d2c71e5bfd3f1b682bffcd76b6538b176b2ff78
wilsontomas <wilsonpumm@gmail.com>
2021-08-08 11:43:23 -0400

    Se creo la aplicacion de wf

---
+++ WFAppCRUD/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAppCRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-V32QJTJ\\SQLEXPRESS;Initial Catalog=Noticias;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                /*  conexion.Open();
+                  SqlCommand comando = new SqlCommand("InsertarNoticia", conexion);
+                  comando.CommandType = CommandType.StoredProcedure;
+                  comando.Parameters.AddWithValue("@Titulo",Titulo.Text);
+                  comando.Parameters.AddWithValue("@Articulo",Articulo.Text);
+                  comando.Parameters.AddWithValue("@CategoriaId",Categoria.SelectedItem);
+                  comando.Parameters.AddWithValue("@PaisId",Pais.SelectedItem);
+                  comando.ExecuteNonQuery();
+                  conexion.Close();*/
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WFAppCRUD/Models/ArticuloNoticias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFAppCRUD.Models
+{
+    class ArticuloNoticias
+    {
+        public int IdNoticias { get; set; }
+        public string Titulo { get; set; }
+        public string Articulo { get; set; }
+        public DateTime Fecha { get; set; }
+        public string NombreCategoria { get; set; }
+        public string NombrePais { get; set; }
+    }
+}
